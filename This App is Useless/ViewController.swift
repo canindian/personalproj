250a9954a7eaafbd83842f8db8f6bafed89cdbee
canindian13 <neelgpatel13@gmail.com>
2019-09-16 14:20:44 -0400

    Initial Commit

---
+++ This App is Useless/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  This App is Useless
+//
+//  Created by Neel Patel on 9/16/19.
+//  Copyright © 2019 Neel Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
